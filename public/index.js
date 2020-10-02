4a0bd29cd76d39af37bb3e47cc87e4ab0612d3e3
Saulo-de-Souza <saulomouradesouza@gmail.com>
2020-10-02 00:55:40 -0300

    testando ie

--- public/index.js
+++ public/index.js
@@ -1,15 +1,13 @@
 // Evento ao carregar o hmtl
-window.onload = () => {
+window.onload = function () {
   var swRegistration;
 
-  // var host = 'http://localhost:3000';
-  var host = 'https://saullosouza-com.umbler.net';
   var user = '';
   var color = '';
 
   if ('caches' in window) {
-    caches.open('usuario').then((cache) => {
-      cache.keys().then((arrayOfRequest) => {
+    caches.open('usuario').then(function (cache) {
+      cache.keys().then(function (arrayOfRequest) {
         if (arrayOfRequest.length > 0) {
           user = arrayOfRequest[0].url.replace(host + '/', '');
           document.getElementById('username').value = user;
@@ -17,8 +15,8 @@ window.onload = () => {
       });
     });
 
-    caches.open('cor').then((ca) => {
-      ca.keys().then((re) => {
+    caches.open('cor').then(function (ca) {
+      ca.keys().then(function (re) {
         if (re.length > 0) {
           color = re[0].url.replace(host + '/', '');
           document.getElementById(re[0].url.replace(host + '/', '')).checked = true;
@@ -30,11 +28,9 @@ window.onload = () => {
   // Permissão
   if (!('Notification' in window)) {
     console.error('This browser does not support desktop notification');
-  }
-  else if (Notification.permission === 'granted') {
+  } else if (Notification.permission === 'granted') {
     console.log('Permission to receive notifications has been granted');
-  }
-  else if (Notification.permission !== 'denied') {
+  } else if (Notification.permission !== 'denied') {
     Notification.requestPermission(function (permission) {
       if (permission === 'granted') {
         console.log('Permission to receive notifications has been granted');
@@ -52,54 +48,69 @@ window.onload = () => {
   }
 
   // Evento que recebe as mensagens
-  socket.on('receivedMessage', (data) => {
+  socket.on('receivedMessage', function (data) {
     renderMessage(data);
     notify(data.author, data.message);
   });
 
   // Evento que escreve todas as mensagens salvas no servidor ao conectar
-  socket.on('previousMessages', (data) => {
-    for (message of data) {
-      renderMessage(message);
+  socket.on('previousMessages', function (data) {
+    for (x = 0; x < data.length; x++) {
+      renderMessage(data[x]);
     }
+    // for (message of data) {
+    //   renderMessage(message);
+    // }
   });
 
-  socket.on('chave', (data) => {
+  socket.on('chave', function (data) {
+    console.log(data);
+
     window.vapidPublicKey = new Uint8Array(data.valor);
 
     if ('serviceWorker' in navigator) {
       console.log('ServiceWorker é suportado, vamos usar!');
       navigator.serviceWorker
-        .register('sw.js', {
-          scope: '/',
-        })
-        .then((reg) => {
+        .register('sw.js')
+        .then(function (reg) {
+          console.log('Service worker registrado');
+
           swRegistration = reg;
 
-          swRegistration.pushManager.getSubscription().then((res) => {
-            if (res != undefined) {
-              res.unsubscribe().then(() => {
-                swRegistration.pushManager.subscribe({ userVisibleOnly: true, applicationServerKey: data.valor }).then((res) => {
-                  console.log(res);
+          navigator.serviceWorker.ready.then(function (serviceWorkerRegistration) {
+            console.log('navigator.serviceWorker.ready ok');
+
+            swRegistration.pushManager.getSubscription().then(function (res1) {
+              console.log(res1 == null);
+
+              if (res1 == undefined || res1 == null) {
+                navigator.serviceWorker.ready.then(function (serviceWorkerRegistration) {
+                  swRegistration.pushManager.subscribe({ userVisibleOnly: true, applicationServerKey: data.valor }).then(function (res3) {
+                    console.log('PushNotification registrado');
+                  });
                 });
-              });
-            } else {
-              navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
-                serviceWorkerRegistration.pushManager.subscribe({ userVisibleOnly: true, applicationServerKey: data.valor }).then((res) => {
-                  console.log(res);
+              } else {
+                res1.unsubscribe().then(function () {
+                  console.log('PushNotification removido');
+
+                  swRegistration.pushManager.subscribe({ userVisibleOnly: true, applicationServerKey: data.valor }).then(function (res2) {
+                    console.log('PushNotification registrado');
+                  });
                 });
-              });
-            }
+              }
+            });
           });
         })
-        .catch((err) => console.error('error registering sw', err));
+        .catch(function (err) {
+          console.error('error registering sw', err);
+        });
     } else {
       console.log('ServiceWorker não é suportado.');
     }
   });
 
   // Tratando o submit do formulário do html
-  document.getElementById('chat').addEventListener('submit', (event) => {
+  document.getElementById('chat').addEventListener('submit', function (event) {
     event.preventDefault();
 
     var author = document.getElementById('username').value;
@@ -107,10 +118,10 @@ window.onload = () => {
 
     // Verificando se há usuário e mensagem
     if (author.length && message.length) {
-      navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
+      navigator.serviceWorker.ready.then(function (serviceWorkerRegistration) {
         serviceWorkerRegistration.pushManager
           .getSubscription()
-          .then((subscription) => {
+          .then(function (subscription) {
             console.log('subscription', subscription);
 
             var messagObject = {
@@ -122,18 +133,18 @@ window.onload = () => {
             };
 
             if ('caches' in window) {
-              caches.open('usuario').then((cache) => {
-                cache.delete(user).then((res) => {
-                  cache.add(document.getElementById('username').value).then((res2) => {
+              caches.open('usuario').then(function (cache) {
+                cache.delete(user).then(function (res) {
+                  cache.add(document.getElementById('username').value).then(function (res2) {
                     user = document.getElementById('username').value;
                   });
                 });
               });
 
               if (document.getElementById('red').checked || document.getElementById('blue').checked || document.getElementById('pink').checked) {
-                caches.open('cor').then((cache) => {
-                  cache.delete(color).then((res) => {
-                    cache.add(document.getElementById('red').checked ? 'red' : document.getElementById('blue').checked ? 'blue' : document.getElementById('pink').checked ? 'pink' : '').then((res2) => {
+                caches.open('cor').then(function (cache) {
+                  cache.delete(color).then(function (res) {
+                    cache.add(document.getElementById('red').checked ? 'red' : document.getElementById('blue').checked ? 'blue' : document.getElementById('pink').checked ? 'pink' : '').then(function (res2) {
                       color = document.getElementById('red').checked ? 'red' : document.getElementById('blue').checked ? 'blue' : document.getElementById('pink').checked ? 'pink' : '';
                     });
                   });
@@ -150,7 +161,9 @@ window.onload = () => {
             document.getElementById('message').value = '';
             document.getElementById('message').focus();
           })
-          .catch((err) => console.error(err));
+          .catch(function (err) {
+            console.error(err);
+          });
       });
     } else {
       alert('Para enviar mensagem, deve ter pelo menos um NOME e uma MENSAGEM!');
@@ -163,7 +176,7 @@ window.onload = () => {
       var notification = new Notification(author, {
         icon: './img/s1.ico',
         body: message,
-        vibrate: true,
+        vibrate: [300, 100, 400, 100, 400, 100, 400],
         image: './img/s1.ico',
       });
       notification.onclick = function () {
--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,8 @@
-'use strict';
+console.log('lendo arquivo sw.js');
+
+// var host = 'http://localhost:3000';
+var host = 'https://saullosouza-com.umbler.net';
+
 // self.addEventListener('install', function (e) {
 //   e.waitUntil(
 //     caches.open('airhorner').then(function (cache) {
@@ -7,13 +11,34 @@
 //   );
 // });
 
-self.addEventListener('push', (event) => {
+self.addEventListener('push', function(event)  {
   console.log('json', event.data.json());
   // console.log('text', event.data.text());
-  let title = event.data.json().author || 'Yay a message';
-  let body = event.data.json().message;
-  let tag = 'push-simple-demo-notification-tag' + new Date().getMilliseconds();
-  let icon = './img/s1.ico';
+  var title = event.data.json().author || 'Yay a message';
+  var body = event.data.json().message;
+  var tag = 'push-simple-demo-notification-tag' + new Date().getMilliseconds();
+  var icon = './img/s1.ico';
+  var actions = [{ action: 'open_url', title: 'Abrir' }];
+  var data = { url: host };
 
-  event.waitUntil(self.registration.showNotification(title, { body, icon, tag }));
+  event.waitUntil(self.registration.showNotification(title, { body, icon, tag, actions, data }));
+
+ 
 });
+
+self.addEventListener(
+  'notificationclick',
+  function (event) {
+    switch (event.action) {
+      case 'open_url':
+        clients.openWindow(event.notification.data.url, '_self', 'toolbar=no, scrollbars=no, resizable=no, width=426, height=640, location=no, status=no, top=150, left=100, titlebar=no, channelmode=yes'); //which we got from above
+        break;
+      case 'any_other_action':
+        clients.openWindow('https://www.example.com');
+        break;
+    }
+  },
+  false
+);
+
+console.log('evento push e pushclick dentro do sw registrado');
